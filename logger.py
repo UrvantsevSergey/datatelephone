597dcf194d3142c835478d439f5b6534ca274b67
UrvantsevSergey <dbktmn@gmail.com>
2024-06-10 12:26:37 +0500

    add rename data for sec var

--- logger.py
+++ logger.py
@@ -45,58 +45,98 @@ def print_data():
         data_second = f.readlines()
         print(*data_second)
         
+
+# ДОМАШНЕЕ ЗАДАНИЕ 
+
 def delete_data():
+    # Выбираем с каким справочников работаем
     s = int(input("Выберите справочник: \n 1 - 1 справочник \n 2 - 2 справочник \n"))
     while s != 1 and s != 2:
         print("Неправильный ввод")
         s = int(input("Выберите справочник: \n 1 - 1 справочник \n 2 - 2 справочник \n"))
     if s == 1:
+    # Выводим пронумерованные данные из 1 файла
         print('Вывожу данные из 1 файла: \n')
         with open('data_first_var.csv', 'r+', encoding='utf-8') as f:
             data_first = f.readlines()
             for i, line in enumerate((data_first)):
                 print(f'{i+1} {line.strip()}')
+    # Проверка на наличие данных по номеру строки
             n = int(input("Выберите номер строки для удаления: "))
             while n < 0 or n > len(data_first):
                 print("Неправильный ввод")
                 n = int(input("Выберите номер строки для удаления: "))
+    # Удаляем строку
         with open("data_first_var.csv", "w", encoding="utf-8") as f:
             for i, line in enumerate(data_first):
                 if i+1 != n:
                     f.write(line)
-                                        
+    if s == 2:
+    # Выводим пронумерованные данные из 2 файла
         print('Вывожу данные из 2 файла: \n')
         with open('data_sec_var.csv', 'r+', encoding='utf-8') as f:
             data_first = f.readlines()
             for i, line in enumerate((data_first)):
                 print(f'{i+1} {line.strip()}')
+    # Проверка на наличие данных по номеру строки
             n = int(input("Выберите номер строки для удаления: "))
             while n < 0 or n > len(data_first):
                 print("Неправильный ввод")
                 n = int(input("Выберите номер строки для удаления: "))
+    # Удаляем строку
         with open("data_sec_var.csv", "w", encoding="utf-8") as f:
             for i, line in enumerate(data_first):
                 if i+1 != n:
                     f.write(line)
 
 def rename_data():
+    # Выбираем с каким справочником работаем
     s = int(input("Выберите справочник: \n 1 - 1 справочник \n 2 - 2 справочник \n"))
     while s != 1 and s != 2:
         print("Неправильный ввод")
         s = int(input("Выберите справочник: \n 1 - 1 справочник \n 2 - 2 справочник \n"))
+
     if s == 1:
+        # Выводим пронумерованные данные из файла
         print('Вывожу данные из 1 файла: \n')
         with open('data_first_var.csv', 'r+', encoding='utf-8') as f:
             data_first = f.readlines()
             for i, line in enumerate(data_first):
                 print(f'{i+1} {line.strip()}')
+        # Проверка на наличие данных по номеру строки
             n = int(input("Выберите номер строки для изменения: "))
             while n < 0 or n > len(data_first):
                 print("Неправильный ввод")
                 n = int(input("Выберите номер строки для изменения: "))
+        # Вводим новые данные
             r = input("Введите новые данные: ")
-
+        # Записываем новые данные в файл
         with open("data_first_var.csv", "w", encoding="utf-8") as f:
+            for i, line in enumerate(data_first):
+                if i+1 == n:
+                    data_first[i] = r + '\n'
+                    f.writelines(data_first)
+    if s == 2:
+        # Выводим пронумерованные данные из файла
+        print('Вывожу данные из 2 файла: \n')
+        with open('data_sec_var.csv', 'r+', encoding='utf-8') as f:
+            data_first = f.readlines()
+            for i, line in enumerate(data_first):
+                print(f'{i+1} {line.strip()}')
+        # Проверка на наличие данных по номеру строки
+            n = int(input("Выберите номер строки для изменения: "))
+            while n < 0 or n > len(data_first):
+                print("Неправильный ввод")
+                n = int(input("Выберите номер строки для изменения: "))
+        # Вводим новые данные используя функции name_data, surname_data, phone_data, address_data
+            print("Введите новые данные")
+            name = name_data()
+            surname = surname_data()
+            phone = phone_data()
+            address = address_data()
+            r = f"{name};{surname};{phone};{address}"
+        # Записываем новые данные в файл
+        with open("data_sec_var.csv", "w", encoding="utf-8") as f:
             for i, line in enumerate(data_first):
                 if i+1 == n:
                     data_first[i] = r + '\n'
